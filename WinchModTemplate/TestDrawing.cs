b9b715a66b1e9f0ea3aa11d8acde8630f8d73e84
Pumpkin-Cake <pumpkincake95@gmail.com>
2025-04-20 23:23:36 -0500

    0.0.1

---
+++ WinchModTemplate/TestDrawing.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using Winch.Core;
+
+namespace TestDrawing
+{
+	public class TestDrawing : MonoBehaviour
+	{
+		public void Awake()
+		{
+			WinchCore.Log.Debug($"{nameof(TestDrawing)} has loaded!");
+		}
+	}
+}
